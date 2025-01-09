f0c489f9cf4485867dc11af5c6b626eb7fc8f4cb
ggaillard <gaillard42@gmail.com>
2025-01-09 08:34:14 +0100

    Update job.py

--- job.py
+++ job.py
@@ -1,301 +1,62 @@
-# Dictionnaire des métiers et attitudes
-job = {
-    "Support et administration IT": {
-        "Technicien(ne) support informatique": {
-            "Compétences techniques": [
-                "Configurer des équipements réseau",
-                "Diagnostiquer les dysfonctionnements systèmes",
-                "Documenter les procédures techniques",
-                "Travailler sur le terrain",
-                "Gérer les priorités des interventions",
-                "Optimiser les performances des systèmes"
-            ],
-            "Compétences comportementales": [
-                "Aimer résoudre des problèmes techniques",
-                "Être patient(e) et pédagogue",
-                "Rigueur dans la gestion des infrastructures",
-                "Travail en équipe",
-                "Avoir une bonne capacité d'écoute",
-                "Aimer travailler en contact direct avec les utilisateurs",
-                "Être méthodique dans la résolution des problèmes",
-                "Savoir gérer le stress des utilisateurs"
-            ]
-        },
-        "Technicien(ne) systèmes et réseaux": {
-            "Compétences techniques": [
-                "Configurer des équipements réseau",
-                "Diagnostiquer les dysfonctionnements systèmes",
-                "Avoir un esprit logique et méthodique",
-                "Documenter les procédures techniques",
-                "Travailler sur le terrain",
-                "Gérer les priorités des interventions",
-                "Optimiser les performances des systèmes"
-            ],
-            "Compétences comportementales": [
-                "Rigueur dans la gestion des infrastructures",
-                "Travail en équipe",
-                "Curiosité pour les nouvelles technologies"
-            ]
-        },
-        "Administrateur(trice) systèmes et réseaux": {
-            "Compétences techniques": [
-                "Assurer la disponibilité des systèmes",
-                "Maîtriser les environnements serveurs",
-                "Sécuriser les infrastructures informatiques",
-                "Réactivité face aux incidents critiques",
-                "Méthodique dans la gestion des mises à jour"
-            ],
-            "Compétences comportementales": [
-                "Responsable et autonome",
-                "Travail en équipe sur des projets complexes",
-                "Documenter les processus et configurations",
-                "Curiosité pour les nouvelles tendances en IT",
-                "Esprit analytique pour résoudre les problèmes"
-            ]
-        }
+jobs = {
+    "Développement": {
+        "Développeur Frontend": [
+            "Aimez-vous le design ?",
+            "Aimez-vous JavaScript ?",
+            "Avez-vous une bonne maîtrise des langages front-end ?",
+            "Savez-vous optimiser les performances des sites ?",
+            "Savez-vous travailler en équipe avec des designers ?",
+            "Aimez-vous créer des interfaces utilisateur attrayantes ?",
+            "Êtes-vous curieux(se) des tendances web ?",
+            "Aimez-vous travailler sur des projets interactifs ?",
+            "Êtes-vous rigoureux(se) dans le respect des standards web ?",
+            "Avez-vous un bon sens esthétique ?",
+            "Êtes-vous patient(e) dans la résolution des problèmes ?",
+            "Aimez-vous apprendre de nouveaux outils ?"
+        ],
+        "Développeur Backend": [
+            "Aimez-vous les bases de données ?",
+            "Aimez-vous Python ?",
+            "Avez-vous une bonne maîtrise des langages backend ?",
+            "Savez-vous optimiser les performances des serveurs ?",
+            "Savez-vous travailler en équipe avec des développeurs frontend ?",
+            "Aimez-vous créer des API ?",
+            "Êtes-vous curieux(se) des nouvelles technologies backend ?",
+            "Aimez-vous travailler sur des projets complexes ?",
+            "Êtes-vous rigoureux(se) dans le respect des standards de sécurité ?",
+            "Avez-vous un bon sens de l'architecture logicielle ?",
+            "Êtes-vous patient(e) dans la résolution des problèmes ?",
+            "Aimez-vous apprendre de nouveaux outils ?"
+        ]
     },
-    "Développement et programmation": {
-        "Développeur(se) d'applications": {
-            "Compétences techniques": [
-                "Rigueur dans l'écriture du code",
-                "Analyser les besoins des utilisateurs",
-                "Tester et corriger les bugs efficacement",
-                "Organisation dans la gestion des projets"
-            ],
-            "Compétences comportementales": [
-                "Créer des logiciels innovants",
-                "Curiosité pour les nouvelles technologies",
-                "Résoudre des problèmes complexes",
-                "Travail en équipe avec d'autres développeurs",
-                "Esprit créatif pour proposer des solutions",
-                "Patience face aux défis techniques"
-            ]
-        },
-        "Développeur(se) web": {
-            "Compétences techniques": [
-                "Maîtriser les langages front-end",
-                "Optimiser les performances des sites",
-                "Travailler en équipe avec des designers"
-            ],
-            "Compétences comportementales": [
-                "Créer des interfaces utilisateur attrayantes",
-                "Curiosité pour les tendances web",
-                "Travailler sur des projets interactifs",
-                "Rigueur dans le respect des standards web",
-                "Bon sens esthétique",
-                "Patience dans la résolution des problèmes",
-                "Apprendre de nouveaux outils"
-            ]
-        },
-        "Développeur(se) mobile": {
-            "Compétences techniques": [
-                "Maîtriser les langages de programmation mobile (Swift, Kotlin, etc.)",
-                "Optimiser les performances des applications mobiles",
-                "À l'aise avec les environnements de développement mobile"
-            ],
-            "Compétences comportementales": [
-                "Créer des applications innovantes",
-                "Curiosité pour les nouvelles tendances mobiles",
-                "Résoudre des problèmes complexes",
-                "Travail en équipe avec des designers et développeurs"
-            ]
-        },
-        "Intégrateur(trice) web": {
-            "Compétences techniques": [
-                "Maîtriser les langages de balisage et de style (HTML, CSS)",
-                "Intégrer des éléments graphiques et techniques",
-                "À l'aise avec les outils de gestion de contenu"
-            ],
-            "Compétences comportementales": [
-                "Assembler des éléments pour créer des sites web",
-                "Rigueur dans le respect des standards web",
-                "Travail en équipe avec des designers et développeurs"
-            ]
-        },
-        "Analyste programmeur(se)": {
-            "Compétences techniques": [
-                "Analyser les besoins des utilisateurs",
-                "Maîtriser les langages de programmation",
-                "Tester et corriger les bugs"
-            ],
-            "Compétences comportementales": [
-                "Résoudre des problèmes complexes",
-                "Curiosité pour les nouvelles technologies",
-                "Travail en équipe"
-            ]
-        },
-        "Spécialiste en traitement du langage naturel (NLP)": {
-            "Compétences techniques": [
-                "Maîtriser les techniques de traitement du langage naturel",
-                "Développer des modèles de NLP",
-                "À l'aise avec les outils de NLP"
-            ],
-            "Compétences comportementales": [
-                "Résoudre des problèmes complexes",
-                "Curiosité pour les nouvelles technologies",
-                "Travail en équipe"
-            ]
-        }
-    },
-    "Infrastructure et gestion des données": {
-        "Architecte de données": {
-            "Compétences techniques": [
-                "Concevoir et optimiser la structure des données",
-                "Savoir gérer les bases de données",
-                "Être à l'aise avec les outils de gestion de données"
-            ],
-            "Compétences comportementales": [
-                "Aimer résoudre des problèmes complexes",
-                "Être curieux(se) des nouvelles technologies",
-                "Savoir travailler en équipe"
-            ]
-        },
-        "Ingénieur(e) Big Data": {
-            "Compétences techniques": [
-                "Gérer et exploiter de très grands volumes de données",
-                "Savoir utiliser les outils de Big Data",
-                "Être à l'aise avec les bases de données distribuées"
-            ],
-            "Compétences comportementales": [
-                "Aimer résoudre des problèmes complexes",
-                "Être curieux(se) des nouvelles technologies",
-                "Savoir travailler en équipe"
-            ]
-        }
-    },
-    "Ingénieur(e) en machine learning": {
-            "Compétences techniques": [
-                "Maîtriser les algorithmes d'apprentissage automatique",
-                "Savoir traiter et analyser de grandes quantités de données",
-                "Être à l'aise avec les outils de machine learning"
-            ],
-            "Compétences comportementales": [
-                "Aimer résoudre des problèmes complexes",
-                "Être curieux(se) des nouvelles technologies",
-                "Savoir travailler en équipe"
-            ]
-        },
-        "Ingénieur(e) en data science": {
-            "Compétences techniques": [
-                "Maîtriser les techniques de traitement des données",
-                "Savoir extraire des insights à partir de données",
-                "Être à l'aise avec les outils de data science"
-            ],
-            "Compétences comportementales": [
-                "Aimer résoudre des problèmes complexes",
-                "Être curieux(se) des nouvelles technologies",
-                "Savoir travailler en équipe"
-            ]
-        },
-        "Data scientist": {
-            "Compétences techniques": [
-                "Maîtriser les techniques de traitement des données",
-                "Savoir développer des modèles prédictifs",
-                "Être à l'aise avec les outils de data science"
-            ],
-            "Compétences comportementales": [
-                "Aimer résoudre des problèmes complexes",
-                "Être curieux(se) des nouvelles technologies",
-                "Savoir travailler en équipe"
-            ]
-        },
-        "Data analyst": {
-            "Compétences techniques": [
-                "Maîtriser les techniques d'analyse des données",
-                "Savoir utiliser les outils de visualisation de données",
-                "Être à l'aise avec les bases de données"
-            ],
-            "Compétences comportementales": [
-                "Aimer résoudre des problèmes complexes",
-                "Être curieux(se) des nouvelles technologies",
-                "Savoir travailler en équipe"
-            ]
-        },
-        "Ingénieur(e) en intelligence artificielle": {
-            "Compétences techniques": [
-                "Maîtriser les techniques d'intelligence artificielle",
-                "Savoir développer des systèmes intelligents",
-                "Être à l'aise avec les outils d'IA"
-            ],
-            "Compétences comportementales": [
-                "Aimer résoudre des problèmes complexes",
-                "Être curieux(se) des nouvelles technologies",
-                "Savoir travailler en équipe"
-            ]
-        }
-    },
-    "Sécurité informatique et conformité": {
-        "Consultant(e) en sécurité informatique": {
-            "Compétences techniques": [
-                "Évaluer les vulnérabilités",
-                "Proposer des stratégies de sécurité",
-                "Être à l'aise avec les outils de sécurité"
-            ],
-            "Compétences comportementales": [
-                "Aimer résoudre des problèmes complexes",
-                "Être curieux(se) des nouvelles technologies",
-                "Savoir travailler en équipe"
-            ]
-        },
-        "Technicien(ne) en cybersécurité": {
-            "Compétences techniques": [
-                "Mettre en place et gérer les outils de protection",
-                "Savoir diagnostiquer les incidents de sécurité",
-                "Être à l'aise avec les environnements de sécurité"
-            ],
-            "Compétences comportementales": [
-                "Aimer résoudre des problèmes complexes",
-                "Être curieux(se) des nouvelles technologies",
-                "Savoir travailler en équipe"
-            ]
-        },
-        "Pentester (Testeur d’intrusion)": {
-            "Compétences techniques": [
-                "Simuler des cyberattaques",
-                "Identifier les failles de sécurité",
-                "Être à l'aise avec les outils de test d'intrusion"
-            ],
-            "Compétences comportementales": [
-                "Aimer résoudre des problèmes complexes",
-                "Être curieux(se) des nouvelles technologies",
-                "Savoir travailler en équipe"
-            ]
-        },
-        "Responsable sécurité des systèmes d'information (RSSI)": {
-            "Compétences techniques": [
-                "Définir et mettre en œuvre la politique de sécurité",
-                "Savoir gérer les incidents de sécurité",
-                "Être à l'aise avec les environnements de sécurité"
-            ],
-            "Compétences comportementales": [
-                "Aimer résoudre des problèmes complexes",
-                "Être curieux(se) des nouvelles technologies",
-                "Savoir travailler en équipe"
-            ]
-        },
-        "Délégué(e) à la protection des données (DPO)": {
-            "Compétences techniques": [
-                "Superviser la conformité aux réglementations",
-                "Savoir gérer les données personnelles",
-                "Être à l'aise avec les outils de gestion de données"
-            ],
-            "Compétences comportementales": [
-                "Aimer résoudre des problèmes complexes",
-                "Être curieux(se) des nouvelles technologies",
-                "Savoir travailler en équipe"
-            ]
-        },
-        "Consultant(e) en conformité RGPD": {
-            "Compétences techniques": [
-                "Conseiller les entreprises sur le RGPD",
-                "Savoir gérer les données personnelles",
-                "Être à l'aise avec les outils de gestion de données"
-            ],
-            "Compétences comportementales": [
-                "Aimer résoudre des problèmes complexes",
-                "Être curieux(se) des nouvelles technologies",
-                "Savoir travailler en équipe"
-            ]
-        }
+    "Data Science": {
+        "Data Analyst": [
+            "Aimez-vous analyser des données ?",
+            "Aimez-vous SQL ?",
+            "Avez-vous une bonne maîtrise des outils de visualisation de données ?",
+            "Savez-vous optimiser les requêtes SQL ?",
+            "Savez-vous travailler en équipe avec des data scientists ?",
+            "Aimez-vous créer des rapports de données ?",
+            "Êtes-vous curieux(se) des tendances en analyse de données ?",
+            "Aimez-vous travailler sur des projets de données ?",
+            "Êtes-vous rigoureux(se) dans le respect des standards de qualité des données ?",
+            "Avez-vous un bon sens de l'analyse ?",
+            "Êtes-vous patient(e) dans la résolution des problèmes ?",
+            "Aimez-vous apprendre de nouveaux outils ?"
+        ],
+        "Data Scientist": [
+            "Aimez-vous les statistiques ?",
+            "Aimez-vous Python ?",
+            "Avez-vous une bonne maîtrise des algorithmes de machine learning ?",
+            "Savez-vous optimiser les modèles de machine learning ?",
+            "Savez-vous travailler en équipe avec des data analysts ?",
+            "Aimez-vous créer des modèles prédictifs ?",
+            "Êtes-vous curieux(se) des nouvelles technologies en data science ?",
+            "Aimez-vous travailler sur des projets de machine learning ?",
+            "Êtes-vous rigoureux(se) dans le respect des standards de qualité des modèles ?",
+            "Avez-vous un bon sens de l'analyse statistique ?",
+            "Êtes-vous patient(e) dans la résolution des problèmes ?",
+            "Aimez-vous apprendre de nouveaux outils ?"
+        ]
     }
 }
